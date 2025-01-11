f8f1b6969642b298748848189a00ffadecf26189
Pawan Rama Mali <prm@outlook.in>
2025-01-11 23:53:43 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="neurotune",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "torch>=1.8.0",
+        "numpy>=1.19.2",
+        "tqdm>=4.50.0",
+    ],
+    author="Pawan Rama Mali",
+    author_email="your.email@example.com",
+    description="A powerful library for neural network fine-tuning",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/PawanRamaMali/NeuroTune",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
