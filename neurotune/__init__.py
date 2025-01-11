7ce8b9964ed3c3604c5972fbac360f90a83158d1
Pawan Rama Mali <prm@outlook.in>
2025-01-12 00:02:47 +0530

    Update __init__.py

--- neurotune/__init__.py
+++ neurotune/__init__.py
@@ -1,3 +1,14 @@
 from .optimizers import OptiBrain, AdaptiveMomentum, ElasticLR
 from .metrics import ConvergenceTracker, LossAnalyzer
 from .utils import setup_logging, initialize_parameters
+
+__version__ = "0.1.0"
+__all__ = [
+    "OptiBrain",
+    "AdaptiveMomentum",
+    "ElasticLR",
+    "ConvergenceTracker",
+    "LossAnalyzer",
+    "setup_logging",
+    "initialize_parameters",
+]
